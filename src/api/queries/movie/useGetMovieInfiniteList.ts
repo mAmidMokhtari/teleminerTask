d87ff97ccfc939ee1dfde2b23f400cc252c5e05a
mAmidMokhtari <m.amid.mokhtari@gmail.com>
2025-07-06 18:45:15 +0330

    update task

--- src/api/queries/movie/useGetMovieInfiniteList.ts
+++ src/api/queries/movie/useGetMovieInfiniteList.ts
@@ -3,16 +3,25 @@ import { Movie } from "@/model";
 import { Paginate } from "@/model/pagination";
 import type { IPaginate } from "@/model/pagination/index.type";
 import api from "@/services";
-import { QueryFunctionContext, useInfiniteQuery } from "@tanstack/react-query";
+import {
+  type QueryFunctionContext,
+  useInfiniteQuery,
+} from "@tanstack/react-query";
 
 export const useGetMovieInfiniteList = (search: string, genreId: string[]) => {
-  const queryFn = async ({
-    pageParam = 1,
-    queryKey,
-  }: QueryFunctionContext<[string, string, string]>) => {
+  const genreString = genreId.join(",");
+  const queryKey = ["movies", search, genreString];
+
+  const queryFn = async (
+    ctx: QueryFunctionContext
+  ): Promise<IPaginate<IMovie>> => {
+    const page = (ctx.pageParam ?? 1) as number;
+
+    const [_, search, genres] = ctx.queryKey as [string, string, string];
+
     return api.movie.getMovies({
-      pageParam,
-      queryKey,
+      pageParam: page,
+      queryKey: [_, search, genres],
     });
   };
 
@@ -24,21 +33,21 @@ export const useGetMovieInfiniteList = (search: string, genreId: string[]) => {
     hasNextPage,
     fetchNextPage,
     isFetchingNextPage,
-  } = useInfiniteQuery<IPaginate<IMovie>>({
-    queryKey: ["movies", search, genreId.join(",")],
+  } = useInfiniteQuery<IPaginate<IMovie>, Error>({
+    queryKey,
     queryFn,
-    getNextPageParam: (lastPage) => {
-      return lastPage?.page &&
-        lastPage.total_pages &&
-        lastPage.page < lastPage.total_pages
+    initialPageParam: 1,
+    getNextPageParam: (lastPage) =>
+      typeof lastPage.page === "number" &&
+      typeof lastPage.total_pages === "number" &&
+      lastPage.page < lastPage.total_pages
         ? lastPage.page + 1
-        : undefined;
-    },
+        : undefined,
     staleTime: 5 * 60 * 1000,
   });
 
   const pages = data?.pages?.map(
-    (page) => new Paginate<IMovie, Movie>(Movie, page, page?.page)
+    (page) => new Paginate<IMovie, Movie>(Movie, page, page.page)
   );
 
   return {
